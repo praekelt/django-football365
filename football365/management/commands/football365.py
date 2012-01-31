c6a13ab38927d6e4d2cf1157ade979e56b711235
hedleyroos <hedleyroos@gmail.com>
2012-01-31 14:21:35 +0200

    Setup general form of managemenbt command

--- football365/management/commands/football365.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-
-from football365.models import Call
-
-class Command(BaseCommand):
-    help = "Fetch feeds from Football365 and pass to handlers."
-
-    def handle(self, *args, **options):
-        pass
