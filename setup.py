2f2e0e97d60c41dee7b38c3bce0d89dc9e10c34e
hedleyroos <hedleyroos@gmail.com>
2012-01-31 13:11:33 +0200

    Skeleton

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-football365',
+    version='0.0.1',
+    description='Fetch logs, fixtures, results and live scores via the Football 365 API.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/django-football365',
+    packages = find_packages(),
+    install_requires = [
+        'Django',
+    ],
+    include_package_data=True,
+    tests_require=[
+        'django-setuptest',
+    ],
+    test_suite="setuptest.SetupTestSuite",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
