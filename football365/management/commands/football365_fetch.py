490fdc7881e18d6d9a8bf33a66547ac564de9efb
hedleyroos <hedleyroos@gmail.com>
2012-02-07 15:47:31 +0200

    Fixtures

--- football365/management/commands/football365_fetch.py
+++ football365/management/commands/football365_fetch.py
@@ -1,4 +1,5 @@
 import urllib2
+import datetime
 from lxml import etree
 
 from django.core.management.base import BaseCommand, CommandError
@@ -11,7 +12,8 @@ class Command(BaseCommand):
     help = "Fetch feeds from Football365 and pass to handlers."
 
     pipeline = {
-        'table': ('table_raw', 'xml2dom', 'table_structure')
+        'table': ('table_raw', 'xml2dom', 'table_structure'),
+        'fixtures': ('fixtures_raw', 'xml2dom', 'fixtures_structure'),
     }
 
     @transaction.commit_on_success
@@ -21,14 +23,14 @@ class Command(BaseCommand):
             for handler in self.pipeline[call.call_type]:
                 data = getattr(self, handler)(call, data)
     
-    def _raw(self, service, dt, di):
+    def _raw(self, service, dt, di=None, ci=None):
         """Common method"""
         url = "%s/%s?cl=%s&dt=%s&di=%s" % (
             settings.FOOTBALL365['url'],
             service,
             settings.FOOTBALL365['client_id'],
             dt,
-            di
+            di or ci
         )
         result = ''
         try:
@@ -44,7 +46,7 @@ class Command(BaseCommand):
         return etree.fromstring(data)
 
     def table_raw(self, call, data):
-        return self._raw('tablesfeed', 'TablesFS1', call.football365_service_id)
+        return self._raw('tablesfeed', 'TablesFS1', di=call.football365_service_id)
 
     def table_structure(self, call, data):
         result = []
@@ -62,4 +64,26 @@ class Command(BaseCommand):
                 GOALDIFFERENCE=int(row[8].text),
                 POINTS=int(row[9].text),
             ))
+        return result
+
+    def fixtures_raw(self, call, data):
+        return self._raw('fixturesfeed', 'Fixtures', di=call.football365_service_id)
+
+    def fixtures_structure(self, call, data):
+        result = []
+        for day in data.findall('DAY'):
+            for row in day[0][0][0].findall('MATCH'):
+
+                # We want a UTC-0 time
+                raw_starttime = '%s %s' % (day.get('DATE'), row[3].text)                
+                starttime = datetime.datetime.strptime(raw_starttime, '%d/%m/%Y %H:%M')
+                tz = int(row[3].get('TIMEZONE').replace('GMT', ''))
+                starttime = starttime - datetime.timedelta(hours=tz)
+
+                result.append(dict(
+                    HOMETEAM=row[0].text,
+                    AWAYTEAM=row[1].text,
+                    VENUE=row[2].text,
+                    STARTTIME=starttime
+                ))
         return result
